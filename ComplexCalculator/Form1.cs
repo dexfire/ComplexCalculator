678049c074b21a97f5a2f0bf1f10a1d2057ed83b
dexfire <275176629@qq.com>
2017-10-30 21:40:21 +0800

    WuLvhua first commit

---
+++ ComplexCalculator/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ComplexCalculator
+{
+    public partial class Form1 : Form
+    {
+        ComplexNumber complex1,complex2;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            readvalue();
+            label5.Text=(complex1 + complex2).ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            readvalue();
+            label5.Text = (complex1 - complex2).ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            readvalue();
+            label5.Text = (complex1 * complex2).ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            readvalue();
+            label5.Text = (complex1 / complex2).ToString();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            readvalue();
+            label5.Text = (complex1 ^ int.Parse(textBox3.Text)).ToString();
+        }
+        private void readvalue()
+        {
+            complex1 = new ComplexNumber(int.Parse(textBox1.Text), int.Parse(textBox2.Text));
+            complex2 = new ComplexNumber(int.Parse(textBox3.Text), int.Parse(textBox4.Text));
+        }
+    }
+}
---
+++ ComplexCalculator/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text;
+
+namespace ComplexCalculator
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+            Console.WriteLine("Please input the method you need");
+            Console.WriteLine("\tAdd(+)\n\tSubstract(-)\n\tMultiply(*)\n\tDivide(/)\n\tPowerOnInteger(^)");
+            String method = Console.ReadLine();
+            switch (method)
+            {
+                case "Add":
+                    {
+                        add(); break;
+                    }
+                case "add":
+                    {
+                        add(); break;
+                    }
+                case "+":
+                    {
+                        add(); break;
+                    }
+
+                case "Substract":
+                    {
+                        substract(); break;
+                    }
+                case "substract":
+                    {
+                        substract(); break;
+                    }
+                case "-":
+                    {
+                        substract(); break;
+                    }
+
+                case "Multiply":
+                    {
+                        multiply(); break;
+                    }
+                case "multiply":
+                    {
+                        multiply(); break;
+                    }
+                case "*":
+                    {
+                        multiply(); break;
+                    }
+
+                case "Divide":
+                    {
+                        divide(); break;
+                    }
+                case "divide":
+                    {
+                        divide(); break;
+                    }
+                case "/":
+                    {
+                        divide(); break;
+                    }
+
+                case "Power":
+                    {
+                        power(); break;
+                    }
+                case "power":
+                    {
+                        power(); break;
+                    }
+                case "^":
+                    {
+                        power(); break;
+                    }
+            }
+        }
+
+        private static void power()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void multiply()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void divide()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void substract()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void add()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
